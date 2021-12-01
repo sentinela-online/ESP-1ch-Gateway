ece6fabe8133cb5a9508cd6a093194a1285b85ff
Lucas Sulzbach <lucas@sulzbach.org>
2021-11-30 21:42:30 -0300

    Fix oLED pinout for TTGO board

--- src/oLED.h
+++ src/oLED.h
@@ -41,8 +41,8 @@
 #define OLED_SDA 2								// GPIO2 / D4
 
 #elif _PIN_OUT==4								// TTGO (onboard version used, also for DIY)
-#define OLED_SCL 15								// GPIO15 / 
-#define OLED_SDA 4								// GPIO4 / 
+#define OLED_SCL 22								// GPIO22 / SCL
+#define OLED_SDA 21 							// GPIO21 / SDA
 #define OLED_RST 16								// Reset pin (Some OLED displays do not have it)
 
 #elif _PIN_OUT==5								// TTGO with external OLED
